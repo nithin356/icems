252d59d68f67edb1c4d23f32e1932d06442423e6
nithin <nithinbhandary356@gmail.com>
2018-03-24 23:51:16 +0530

    m

--- view-fest/view-fest.php
+++ view-fest/view-fest.php
@@ -1,6 +1,8 @@
 <?php
 require('connect.php');
 ?>
+
+								
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,26 +16,19 @@ require('connect.php');
 </head>
 
 <body class="fix-sidebar">
-     <!--header.php includes preloader, top navigarion, logo, user dropdown-->
-    <!--div id wrapper in header.php-->
-    <!--left-sidebar.php includes mobile search bar, user profile, menu-->
-    <?php
-	include 'assets/breadcrumbs.php';
-	?>
-        <!-- Page Content -->
-        <div>
+       <!-- Page Content -->
+        <div >
             <div class="container-fluid">
                 <div class="row bg-title">
                     <!-- .page title -->
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Events</h4>
+                        <h4 class="page-title">Fests</h4>
                     </div>
                     <!-- /.page title -->
                     <!-- .breadcrumb -->
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                        <a href="../index.html" target="_blank" class="btn btn-info pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Home</a>
-                       <?php echo breadcrumbs(); ?>
-                    </div>
+                             </div>
                     <!-- /.breadcrumb -->
                 </div>
 				<div class="row">
@@ -45,18 +40,38 @@ require('connect.php');
                 <div class="col-md-4 col-sm-4">
                         <div class="white-box">
                             <div class="row">
-                                  <div class="col-md-8 col-sm-8">
-									College Name:<h3 class="box-title m-b-0"><?php echo $result['cname']; ?></h3>
-                                    <p>  
-                                    Fest Name:<h3 class="box-title m-b-0"><?php echo $result['fname']; ?></h3>
-									 <p>
-									Date:<h3 class="box-title m-b-0"><?php echo $result['date']; ?></h3>
-									  <p>
-									College Name:
-									  <h3 class="box-title m-b-0"><?php echo $result['cname']; ?></h3>
-                                    <p>
-									Fest Description:<h4 class="box-title m-b-0"><?php echo $result['f_desc']; ?></h4>
-								   
-								<a href="view-events.php?id=<?php echo $result['f_id']; ?>" class="fcbtn btn btn-danger model_img">More Inf</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								<a href="../login/" class="fcbtn btn btn-danger model_img">Register</a>
+                                  <div>
+									  
+                                    College Name:<h3 class="box-title m-b-0"><?php echo $result['cname']; ?></h2>  
+                                    Fest Name:<h3 class="box-title m-b-0"><?php echo $result['fname']; ?></h2>  
+                                    Date:<h3 class="box-title m-b-0"><?php echo $result['date']; ?></h2>
+<br/>  
+                                    Description:
+									  <p><?php echo $result['f_desc']; ?>
+										<div class="p-t-5">
+											   
+								<a href="view-events.php?id=<?php echo $result['f_id']; ?>" class="fcbtn btn btn-danger model_img">More Info</a>
+									    </div>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  <?php
+					}
+				  ?>
+
+				</div>
+            </div>
+			<!--footer.php contains footer-->
+            <?php include'assets/footer.php'; ?>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!--jslink has all the JQuery links-->
+    <?php include'assets/jslink.php'; ?>
+</body>
+
+</html>
 							
